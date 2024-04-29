b1baf1d4c1ff499f2438df2074388c357cc61ac2
H3XDaemon <42803553+H3XDaemon@users.noreply.github.com>
2024-04-29 11:50:50 +0800

    Update dark.ps1

--- dark.ps1
+++ dark.ps1
@@ -1,29 +1,40 @@
 $script = {
-    try {
-        $extensions = 'cjpalhdlnbpafiamejdnhcphjbkeiagm','eimadpbcbfnmbkopoojfekhnkhdbieeh'
+    function Set-RegistryValue {
+        param (
+            [string]$Path,
+            [string]$Name,
+            [int]$Value
+        )
+        Set-ItemProperty -Path $Path -Name $Name -Value $Value -Type Dword -Force
+    }
+
+    function Set-ChromeExtension {
+        param (
+            [string]$ExtensionID
+        )
         $key_path = "Software\Policies\Google\Chrome\ExtensionInstallForcelist"
-
         $registry = [Microsoft.Win32.Registry]::LocalMachine.OpenSubKey($key_path, $true)
-
-        $extensions | %{
-            if($null -eq $registry){
-                $registry = [Microsoft.Win32.Registry]::LocalMachine.CreateSubKey($key_path, $true)
-                $registry.SetValue("1", $_)
-            }
-            else{
-                $values = $registry.GetValueNames().ForEach({$registry.GetValue($_)})
-                if($_ -notin $values){
-                    $maximum = $registry.GetValueNames().Where({$_ -match '\d'}) | measure -maximum | select -expand maximum
-                    $maximum += 1
-                    $registry.SetValue($maximum, $_)
-                }
+        if($null -eq $registry){
+            $registry = [Microsoft.Win32.Registry]::LocalMachine.CreateSubKey($key_path, $true)
+            $registry.SetValue("1", $ExtensionID)
+        }
+        else{
+            $values = $registry.GetValueNames().ForEach({$registry.GetValue($_)})
+            if($ExtensionID -notin $values){
+                $maximum = $registry.GetValueNames().Where({$_ -match '\d'}) | measure -maximum | select -expand maximum
+                $maximum += 1
+                $registry.SetValue($maximum, $ExtensionID)
             }
         }
-
-        Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name SystemUsesLightTheme -Value 0 -Type Dword -Force
-        Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name AppsUseLightTheme -Value 0 -Type Dword -Force
-
         $registry.Dispose()
+    }
+
+    try {
+        $extensions = 'cjpalhdlnbpafiamejdnhcphjbkeiagm','eimadpbcbfnmbkopoojfekhnkhdbieeh'
+        $extensions | %{ Set-ChromeExtension -ExtensionID $_ }
+
+        Set-RegistryValue -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion/Themes/Personalize" -Name SystemUsesLightTheme -Value 0
+        Set-RegistryValue -Path "HKCU:\SOFTWARE\Microsoft/Windows/CurrentVersion/Themes/Personalize" -Name AppsUseLightTheme -Value 0
     }
     catch {
         Write-Output $_.Exception.Message
@@ -32,4 +43,4 @@ $script = {
 
 $encodedScript = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($script))
 
-Start-Process PowerShell -ArgumentList "-NoExit", "-EncodedCommand $encodedScript" -Verb RunAs
+Start-Process PowerShell -ArgumentList "-NoExit -EncodedCommand $encodedScript" -Verb RunAs
